cd344a35d73d59da210e2ec1c341223c722f5a66
waelmelhem <waeladli070@gmail.com>
2023-07-10 12:39:39 +0300

    add deep_flatten

---
+++ arrayUtils.js
@@ -0,0 +1,14 @@
+let deep_flatten =function(array){
+    if(!array) return -1;
+    let ans=[];
+    let recu=function(arr){
+        arr.forEach(element=>{
+            if(Array.isArray(element))recu(element);
+            else{
+                ans.push(element)
+            }
+        })
+    }
+    recu(array);
+    return ans;
+}
