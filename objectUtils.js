aa9b64240d8a5b3ea644a49aaa73b0d632440a20
waelmelhem <waeladli070@gmail.com>
2023-07-10 15:29:54 +0300

    get object keys

--- objectUtils.js
+++ objectUtils.js
@@ -5,3 +5,20 @@ let getObjKeys=function(object){
     }
     return ans
 }
+let deep_get=function (object,path="",defualt=null){
+    let path_array=path.split('.');
+    let current_object=object;
+    for(let i =0;i<path_array.length;i++){
+        if(typeof current_object==="" ){
+            return defualt;
+        }
+        let node=path_array[i];
+        if(current_object.hasOwnProperty(node)){
+            current_object=current_object[node];
+        }else{
+            return defualt;
+        }
+
+    }
+    return current_object;
+}
